881ae6c61e2969ae040ea787b05f08396db9c595
Christopher Dunne <christopherdunne@outlook.ie>
2024-06-04 17:56:44 +0000

    Add functions to check pull and decrease pull history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+//function to only load in JS content once the webpage is fully loaded
+//as learned in the Love Maths project
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("pull-1-button").addEventListener('click', rollOnce);
+    document.getElementById("pull-1-button").addEventListener('click', rollTen);
+});
+
 function rollOnce() {
     
 }
@@ -23,16 +30,34 @@ function calculateLuck() {
 
 function displayResults() {}
 
-//function to check how many rolls a user has made to give expected rewards
+//functions to check how many rolls a user has made to give expected rewards
 //or inform them they've made their max of 100
 function checkHistory() {
-    let history = {
-        fullHistory: 0,
-        rareHistory: 0,
-        ultraHistory: 0
-    };
+    fullHistory = parseInt(document.getElementById("fullHistory").innerText);
+    return fullHistory;
 }
 
-function incrementHistory() {}
-function incrementRareHistory() {}
-function incrementUltraHistory() {}
+function checkRareHistory() {
+    rareHistory = parseInt(document.getElementById("rareHistory").innerText);
+    return rareHistory;
+}
+
+function checkUltraHistory() {
+    ultraHistory = parseInt(document.getElementById("ultraHistory").innerText);
+    return ultraHistory;
+}
+
+//functions to update roll counters for giving correct rewards at guartantees
+//and to provide those numbers to the user
+function incrementHistory() {
+    fullHistory = parseInt(document.getElementById("fullHistory").innerText);
+    document.getElementById("fullHistory").innerText = --fullHistory;
+}
+function incrementRareHistory() {
+    rareHistory = parseInt(document.getElementById("rareHistory").innerText);
+    document.getElementById("rareHistory").innerText = --rareHistory;
+}
+function incrementUltraHistory() {
+    ultraHistory = parseInt(document.getElementById("ultraHistory").innerText);
+    document.getElementById("ultraHistory").innerText = --ultraHistory;
+}
