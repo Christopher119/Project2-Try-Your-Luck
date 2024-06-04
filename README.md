856cd98ee245fce2880f36046e6dece6e68fdd1d
Christopher Dunne <christopherdunne@outlook.ie>
2024-06-04 19:45:16 +0000

    Add JavaScript functionality to present results of Rolls

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 https://www.pexels.com/photo/blue-universe-956981/
 
 https://www.pexels.com/photo/stars-1257860/
+
+https://wpshout.com/snippets/css-glow-effect/
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,48 @@
 //as learned in the Love Maths project
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("pull-1-button").addEventListener('click', rollOnce);
-    document.getElementById("pull-1-button").addEventListener('click', rollTen);
+    document.getElementById("pull-10-button").addEventListener('click', rollTen);
     document.getElementById("reset-button").addEventListener('click', resetGame);
 });
 
 function rollOnce() {
-    
+
+    let result = calculateLuck();
+    decrementRareHistory();
+    decrementUltraHistory();
+
+    if(checkHistory() <= 0){
+        alert("You've used all your rolls, please reset and try again!")
+        document.getElementById("fullHistory").innerText = 0;
+        document.getElementById("rareHistory").innerText = 0;
+        document.getElementById("ultraHistory").innerText = 0;
+    }else if(result == "Ultra Rare"){
+        //create a new div to hold and assign the correct result
+        let resultDiv = document.createElement("div");
+        resultDiv.setAttribute("class", "ultra-result");
+        //add the div to the results area
+        document.getElementById("result-area").appendChild(resultDiv);
+        //reset ultra history for logic
+        document.getElementById("ultraHistory").innerText = 100;
+    }else if(result == "Rare"){
+        //create a new div to hold and assign the correct result
+        let resultDiv = document.createElement("div");
+        resultDiv.setAttribute("class", "rare-result");
+        //add the div to the results area
+        document.getElementById("result-area").appendChild(resultDiv);
+        //reset rare history for logic
+        document.getElementById("rareHistory").innerText = 10;
+    }else if(result == "Normal"){
+        //create a new div to hold and assign the correct result
+        let resultDiv = document.createElement("div");
+        resultDiv.setAttribute("class", "normal-result");
+        //add the div to the results area
+        document.getElementById("result-area").appendChild(resultDiv);
+    }else{
+        alert(`error, unknown result: ${result}`);
+    }
+
+    decrementHistory();
 }
 
 //function that calls the rollOnce() function 10 times
@@ -28,16 +64,18 @@ function calculateLuck() {
     let odds = Math.floor(Math.random() * 200) + 1;
     let result;
 
-    if(odds <=100){
+    if(odds <=150){
         result = "Normal";
-    } else if(odds > 100 && odds <= 195){
+    } else if(odds > 151 && odds <= 198){
         result = "Rare";
-    } else if(odds > 195){
+    } else if(odds > 198){
         result = "Ultra Rare";
     } else{
-        console.log("Error: Unexpected result");
+        console.log(`Error: Unexpected result: ${result}`);
         throw "Error: Unexpected result";
     }
+
+    return result;
 }
 
 //function to push show JS modal of results
@@ -45,8 +83,11 @@ function calculateLuck() {
 function displayResults() {}
 
 //function to clear and reset all history
-function resetGame(){
-
+function resetGame() {
+    document.getElementById("result-area").innerHTML = "";
+    document.getElementById("fullHistory").innerText = 100;
+    document.getElementById("rareHistory").innerText = 10;
+    document.getElementById("ultraHistory").innerText = 100;
 }
 
 //functions to check how many rolls a user has made to give expected rewards
@@ -68,15 +109,17 @@ function checkUltraHistory() {
 
 //functions to update roll counters for giving correct rewards at guartantees
 //and to provide those numbers to the user
-function incrementHistory() {
+function decrementHistory() {
     fullHistory = parseInt(document.getElementById("fullHistory").innerText);
     document.getElementById("fullHistory").innerText = --fullHistory;
 }
-function incrementRareHistory() {
+function decrementRareHistory() {
     rareHistory = parseInt(document.getElementById("rareHistory").innerText);
     document.getElementById("rareHistory").innerText = --rareHistory;
 }
-function incrementUltraHistory() {
+function decrementUltraHistory() {
     ultraHistory = parseInt(document.getElementById("ultraHistory").innerText);
     document.getElementById("ultraHistory").innerText = --ultraHistory;
 }
+
+
